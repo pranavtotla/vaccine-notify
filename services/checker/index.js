1abbd7d4d660c5bd468e2e5127d3ebbd0b6e9c88
Pranav Totla <thepranavtotla@gmail.com>
2021-05-03 23:53:30 +0530

    Test complete

--- services/checker/index.js
+++ services/checker/index.js
@@ -12,7 +12,7 @@ const getCentersUnder18 = districtId => {
                 for(const center of centers){
                     const sessions = center.sessions;
                     for(const session of sessions){
-                        if(session.min_age_limit < 46){
+                        if(session.min_age_limit < 18){
                             const data = {
                                 centerName:center.name, 
                                 pincode: center.pincode, 
