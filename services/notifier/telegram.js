17cb7f93f6e14e10be8a19b7fd4f1e2bc91dff55
Pranav Totla <thepranavtotla@gmail.com>
2021-05-03 22:20:51 +0530

    Telegram message send function

--- services/notifier/telegram.js
+++ services/notifier/telegram.js
@@ -1,7 +1,22 @@
+const axios = require('axios');
+const telegramBotToken = require('../../config/config.json').telegram.bot.accesstoken;
+const telegramChannel = require('../../config/config.json').telegram.toChannel.id;
+
 const getMessageForAvailableNotifications = (centerName, pincode, slots, date, price, vaccine) => {
     return `${centerName} - ${pincode} has ${slots} available on ${date} for ${vaccine} ${price} vaccination`;
 };
 
+const sendMessageToChannel = (message) => {
+    const params = {
+        method: 'get',
+        url: `https://api.telegram.org/bot${telegramBotToken}/sendMessage?chat_id=${telegramChannel}&text=${message}`,
+        headers: {
+            'accept': 'application/json',
+        }
+    };
+
+    return axios(params);
+};
+
 module.exports = {
-    getMessageForAvailableNotifications
 };
