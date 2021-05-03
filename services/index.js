c9f13151846cea20c3f99e7f993d222233f495ab
Pranav Totla <thepranavtotla@gmail.com>
2021-05-03 22:25:49 +0530

    Services module

--- services/index.js
+++ services/index.js
@@ -1,9 +1,11 @@
 const config = require('./config');
+const Notifier = require('./notifier');
 const Appointment = require('./appointment');
 const Metadata = require('./metadata');
 
 module.exports = {
     Appointment,
     Metadata,
+    Notifier,
     config
 };
