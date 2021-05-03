7755a63e7ce79eab71e3553757e08ac6ace51179
Pranav Totla <thepranavtotla@gmail.com>
2021-05-03 16:57:35 +0530

    Added URLs

---
+++ services/config.js
@@ -0,0 +1,13 @@
+const rootUrl = 'https://cdn-api.co-vin.in/api';
+const states = '/v2/admin/location/states';
+const districts = '/v2/admin/location/districts/';
+const calendarByPin = '/v2/appointment/sessions/public/calendarByPin';
+const calendarByDistrict = '/v2/appointment/sessions/public/calendarByDistrict';
+
+module.exports = {
+    rootUrl,
+    states,
+    districts,
+    calendarByPin,
+    calendarByDistrict
+};
