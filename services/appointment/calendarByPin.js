39ad2fcb71febd1e5f81717fb24ecb03759b70f0
Pranav Totla <thepranavtotla@gmail.com>
2021-05-11 13:14:20 +0530

    More fixes

--- services/appointment/calendarByPin.js
+++ services/appointment/calendarByPin.js
@@ -8,6 +8,7 @@ const getAppointments = async (pincode, date) => {
         url: `${rootUrl}${calendarByPin}?pincode=${pincode}&date=${date}`,
         headers: {
             'accept': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1'
         }
     };
 
